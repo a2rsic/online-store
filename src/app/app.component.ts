e0197f954ba787a8af55a886733c15c9b472b2b1
Ana <angy.aa@gmail.com>
2020-07-27 00:09:25 +0200

    Add images, create products component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,4 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  public setValue;
-
-  public onClick(value) {
-    if (value === 1) {
-      this.setValue = "products";
-    } else if (value === 2) {
-      this.setValue = "about";
-    } else {
-      this.setValue = "contact";
-    }
-  }
-}
+export class AppComponent {}
