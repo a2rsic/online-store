c61de00d86d33e30db36ecc631e3fd297f734a62
Ana <angy.aa@gmail.com>
2020-07-26 15:05:24 +0200

    Add routing module, set app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { CoreModule } from "src/shared/core/core.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./pages/home/home.component";
+import { MaterialModule } from "src/app/shared/material/material.module";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule],
+  declarations: [AppComponent, HomeComponent, NavbarComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
