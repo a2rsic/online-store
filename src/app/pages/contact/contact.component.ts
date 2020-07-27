50945efeec214a534cddb03b7dd5b1a86c840b62
Ana <angy.aa@gmail.com>
2020-07-27 02:00:05 +0200

    Remove comments

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -6,9 +6,6 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
-  public latitude = 44.801543;
-  public longitude = 20.4519599;
-
   constructor() {}
 
   ngOnInit() {}
