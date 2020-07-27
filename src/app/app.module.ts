dbc38f6b98cfa1503a014f31804f3b04600d7507
Ana <angy.aa@gmail.com>
2020-07-27 23:07:59 +0200

    Add products service and interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { HomeComponent } from "./pages/home/home.component";
 import { MaterialModule } from "src/app/shared/material/material.module";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
-import { ProductsComponent } from './pages/products/products.component';
+import { ProductsComponent } from "./pages/products/products.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent, ProductsComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductsComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
